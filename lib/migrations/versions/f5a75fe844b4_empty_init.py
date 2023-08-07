b8b00f80623b4efe7df1199506d190aa4f422a35
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-08-07 15:21:55 -0700

    adds

---
+++ lib/migrations/versions/f5a75fe844b4_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: f5a75fe844b4
+Revises: 
+Create Date: 2023-08-07 15:14:45.495368
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'f5a75fe844b4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
